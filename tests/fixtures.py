a2ca67e23d4cb89f3b9cece81f967e55c1b48bcd
Inbasu <just.me.juncy@gmail.com>
2024-11-29 15:22:58 +0300

    new file:   tests/fixtures.py 	new file:   tests/test_services/__init__.py 	new file:   tests/test_services/test_connection.py 	new file:   tests/test_services/test_insight.py

---
+++ tests/fixtures.py
@@ -0,0 +1,18 @@
+import os
+
+import pytest
+from dotenv import load_dotenv
+
+from services.connection import Client
+
+
+@pytest.fixture(scope="session")
+def client():
+    load_dotenv()
+    return Client(
+            username=os.getenv("NAME",''), 
+            password=os.getenv("PWORD", ''), 
+            url=os.getenv("URL", ''),
+            client_id=os.getenv("CLIENT_ID", ''), 
+            auth_token=os.getenv("TOKEN", ''),
+            )
---
+++ tests/test_services/test_insight.py
@@ -0,0 +1,14 @@
+import pytest
+
+from services.connection import Client
+from services.insight import Insight
+from services.schemas import GetObjectData
+from tests.fixtures import client  # noqa: F401
+
+
+@pytest.mark.asyncio
+async def test_get_item(client: Client):
+    obj = GetObjectData(scheme=10, object_id=563705)
+    obj = await Insight.get_object(client, data=obj)
+    assert isinstance(obj, dict)
+    print(obj)
