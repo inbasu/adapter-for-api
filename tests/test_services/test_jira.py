f7705a224a5cf85058e543e434959fe42cabc9ff
Inbasu <just.me.juncy@gmail.com>
2024-12-19 17:19:24 +0300

    modified:   tests/test_services/test_jira.py

--- tests/test_services/test_jira.py
+++ tests/test_services/test_jira.py
@@ -7,5 +7,5 @@ from tests.fixtures import jira_api_client as client  # noqa: F401
 
 @pytest.mark.asyncio(loop_scope="session")
 async def test_get_item(client: JiraAPIClient):
-    obj = await Jira.get_issues(client, params={"jql":"key = IT-796953"})
+    obj = await Jira.get_issues(client, params={"jql":"key = IT-796972"})
     print(obj)    
