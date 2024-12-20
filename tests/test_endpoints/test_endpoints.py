85253ccb7fd9e1f4589f660405185736cde0c128
Inbasu <just.me.juncy@gmail.com>
2024-12-20 15:41:28 +0300

    modified:   tests/test_endpoints/test_endpoints.py

--- tests/test_endpoints/test_endpoints.py
+++ tests/test_endpoints/test_endpoints.py
@@ -1,12 +1,12 @@
 import pytest
-from httpx import AsyncClient
+from httpx import ASGITransport, AsyncClient
 
 from main import app
 
 
 @pytest.mark.asyncio(loop_scope="session")
 async def test_get_object():
-    async with AsyncClient(app=app, base_url="http://testserver") as client:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://testserver") as client:
         resp = await client.post('/get', json={"scheme": 10, "object_id": 563705})
         assert resp.status_code == 200
         assert resp.json()["id"] == 563705
@@ -14,14 +14,14 @@ async def test_get_object():
 
 @pytest.mark.asyncio(loop_scope="session")
 async def test_get_objects():
-    async with AsyncClient(app=app, base_url="http://testserver") as client:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://testserver") as client:
         resp = await client.post('/iql', json={"scheme": 10, "iql": "Name like !test"})
         assert resp.status_code == 200
         assert len(resp.json()) > 10
 
 @pytest.mark.asyncio(loop_scope="session")
 async def test_get_issues():
-    async with AsyncClient(app=app, base_url="http://testserver") as client:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://testserver") as client:
         resp = await client.post('/issues', json={"jql": "key=IT-797207"})
         assert resp.status_code == 200
         assert resp.json()[0]["key"] == "IT-797207"
