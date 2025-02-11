1940b9e254ec70c967053b435136f7771943a683
Inbasu <just.me.juncy@gmail.com>
2025-02-11 22:23:39 +0300

    modified:   tests/test_services/test_insight_hooks.py

--- tests/test_services/test_insight_hooks.py
+++ tests/test_services/test_insight_hooks.py
@@ -6,7 +6,7 @@ from tests.fixtures import web_hooks_insight  # noqa: F401
 
 @pytest.mark.web_hooks
 @pytest.mark.asyncio(loop_scope="session")
-async def test_get_items(web_hooks_insight):
+async def test_get_items(web_hooks_insight):  # noqa
     objs = await web_hooks_insight.get_objects(iql="objectId=563705")
     assert isinstance(objs[0], InsightObject)
     assert objs[0].get_field_values("Name")[0].label == "Some test gear"
@@ -14,7 +14,7 @@ async def test_get_items(web_hooks_insight):
 
 @pytest.mark.web_hooks
 @pytest.mark.asyncio(loop_scope="session")
-async def test_get_update_object(web_hooks_insight):
+async def test_get_update_object(web_hooks_insight):  # noqa
     before = (await web_hooks_insight.get_objects(iql="objectId=563705", results=1))[0]
     before_store = before.get_field_values("Store")[0].label
     attrs = {1886: 380820 if before_store == "1014" else 380822}
@@ -26,8 +26,8 @@ async def test_get_update_object(web_hooks_insight):
 
 @pytest.mark.skip(reason="Нет удаления")
 @pytest.mark.asyncio(loop_scope="session")
-async def test_get_create_object(web_hooks_insight):
+async def test_get_create_object(web_hooks_insight):  # noqa
     attrs = {1649: "the test creation#12345", 1890: 381686, 1886: 380822, 1891: 382769, 1892: 383219, 1893: 381031}
-    obj = await web_hooks_insight.create_object(type_id=155, attrs=attrs)
+    obj = await web_hooks_insight.create_object(type_id=150, attrs=attrs)
     assert isinstance(obj, InsightObject)
     assert obj.label == "the test creation"
