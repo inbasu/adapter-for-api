721179da6307cdf512b1a8915b16d3b4dd0099f7
Inbasu <just.me.juncy@gmail.com>
2025-02-17 12:24:21 +0300

    modified:   tests/test_services/test_insight_hooks.py

--- tests/test_services/test_insight_hooks.py
+++ tests/test_services/test_insight_hooks.py
@@ -8,19 +8,19 @@ from tests.fixtures import get_attr, web_hooks_insight  # noqa: F401
 async def test_get_items(web_hooks_insight):  # noqa
     objs = await web_hooks_insight.get_objects(iql="objectId=563705")
     assert isinstance(objs[0], dict)
-    assert objs[0].get_field("Name", [{}])[0].label == "Some test gear"
+    assert get_attr(objs[0], "Name")[0]["label"] == "Some test gear"
 
 
 @pytest.mark.web_hooks
 @pytest.mark.asyncio(loop_scope="session")
 async def test_get_update_object(web_hooks_insight):  # noqa
     before = (await web_hooks_insight.get_objects(iql="objectId=563705", results=1))[0]
-    before_store = before.get_field_values("Store")[0].label
+    before_store = get_attr(before, "Store")[0]["label"]
     attrs = {1886: 380820 if before_store == "1014" else 380822}
     after = await web_hooks_insight.update_object(type_id=155, object_id=563705, attrs=attrs)
     assert isinstance(after, dict)
-    assert before == after
-    assert before_store != after.get("Store", [{}])[0]["label"]
+    assert before["id"] == after["id"]
+    assert before_store != get_attr(after, "Store")[0]["label"]
 
 
 @pytest.mark.skip(reason="Нет удаления")
